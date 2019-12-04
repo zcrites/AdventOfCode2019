f18f2c2f21b111ac59ab1fb99f85b68b70799142
Zachary Crites <z.crites@techsmith.com>
2019-12-04 13:17:12 -0500

    Day4

---
+++ AdventOfCode2019/Day4.fs
@@ -0,0 +1,44 @@
+﻿module Day4
+
+let private MIN = 172930
+let private MAX = 683082
+
+let private quadwise s =
+   s 
+   |> Seq.pairwise |> Seq.pairwise |> Seq.pairwise
+   |> Seq.map( fun (((a,_),(_,_)),((b,c),(_,d))) -> a,b,c,d )
+
+let private isIncreasing n =
+   sprintf "%d" n
+   |> Seq.pairwise 
+   |> Seq.tryFind( fun (a,b) -> a > b )
+   |> Option.isNone
+
+let private hasDouble n =
+   sprintf "%d" n
+   |> Seq.pairwise 
+   |> Seq.tryFind( fun (a,b) -> a = b )
+   |> Option.isSome
+
+let private hasDoubleStrict n =
+   sprintf "_%d_" n
+   |> quadwise
+   |> Seq.tryFind( fun (a,b,c,d) -> a <> b && b = c && c <> d )
+   |> Option.isSome
+
+let private isValid n =
+   isIncreasing n && hasDouble n
+
+let private isValidStrict n = 
+   isIncreasing n && hasDoubleStrict n
+   
+let private countPasswords check =
+   [MIN..MAX]
+   |> Seq.filter( check )
+   |> Seq.length
+
+let part1 () =
+   countPasswords isValid
+
+let part2 () =
+   countPasswords isValidStrict
--- AdventOfCode2019/Program.fs
+++ AdventOfCode2019/Program.fs
@@ -6,7 +6,9 @@ let main argv =
      sprintf "Day 2 Part 1 = %d" <| Day2.part1 ()
      sprintf "Day 2 Part 2 = %d" <| Day2.part2 () 
      sprintf "Day 3 Part 1 = %d" <| Day3.part1 () 
-     sprintf "Day 3 Part 2 = %d" <| Day3.part2 () ]
+     sprintf "Day 3 Part 2 = %d" <| Day3.part2 ()
+     sprintf "Day 4 Part 1 = %d" <| Day4.part1 () 
+     sprintf "Day 4 Part 2 = %d" <| Day4.part2 () ]
    |> Seq.iter (printfn "%s")
 
    System.Console.ReadLine () |> ignore
