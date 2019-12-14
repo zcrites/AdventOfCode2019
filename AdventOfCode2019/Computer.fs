cda37ae9efb00cb613df9bce7d3d3a476d03577a
Unknown <zcrites@umich.edu>
2019-12-14 13:28:28 -0500

    Update computer

--- AdventOfCode2019/Computer.fs
+++ AdventOfCode2019/Computer.fs
@@ -13,80 +13,74 @@ type Computer =
 let read addr m =
     m.Mem |> Map.tryFind addr |> Option.defaultValue 0L
     
-let write addr v m =
+let write addr v m = 
     { m with Mem = m.Mem |> Map.add addr v }
 
-let private jumpTo addr m = 
-    { m with PC = addr }
-
-let private incrementPC v m = 
-    { m with PC = m.PC + v }
+let private jumpTo addr m = { m with PC = addr }
+let private incrementPC v m = { m with PC = m.PC + v }
+let private setInput v m = { m with Input = v }
+let private setOutput v m = { m with Output = v }
 
 let private opcode m = 
     read m.PC m % 100L |> int
 
 let private getParamAddr i m =
     let paramMode = ( read m.PC m / ( pown 10L i  * 10L ) ) % 10L |> int
-
     match paramMode with
     | 0 -> read (m.PC+i) m |> int
     | 1 -> m.PC+i 
     | 2 | _ -> (read (m.PC+i) m |> int) + m.RelOffset
 
-let private binaryOp fn m = 
-    let p1 = m |> getParamAddr 1
-    let p2 = m |> getParamAddr 2
-    let p3 = m |> getParamAddr 3
-    let r = fn (m |> read p1) (m |> read p2)
-    m |> write p3 r |> incrementPC 4
-    
-let private jumpIf predicate m =
-    let p1 = m |> getParamAddr 1
-    let p2 = m |> getParamAddr 2
+let private binaryOp op addr1 addr2 addr3 m =
+    let result = op (m |> read addr1) (m |> read addr2)
+    m |> write addr3 result |> incrementPC 4
 
-    if m |> read p1 |> predicate
-    then m |> jumpTo (m |> read p2 |> int)
+let private jumpIf predicate pAddr1 pAddr2 m =
+    if m |> read pAddr1 |> predicate
+    then m |> jumpTo (m |> read pAddr2 |> int)
     else m |> incrementPC 3
 
-let private exAdd = binaryOp (+)
-let private exMult = binaryOp (*)
-let private exLessThan = binaryOp (fun a b -> if a < b then 1L else 0L)
-let private exEquals = binaryOp (fun a b -> if a = b then 1L else 0L)
-let private jumpIfTrue m = jumpIf ((<>) 0L) m
-let private jumpIfFalse m = jumpIf ((=) 0L) m
+let private add = binaryOp (+)
+let private mult = binaryOp (*)
+let private lt = binaryOp (fun a b -> if a < b then 1L else 0L)
+let private eq = binaryOp (fun a b -> if a = b then 1L else 0L)
+let private jumpTrue = jumpIf ((<>)0L)
+let private jumpFalse = jumpIf ((=)0L)
 
-let private offsetRelativeBase m =
-    let p1 = m |> getParamAddr 1
-    { m with RelOffset = m.RelOffset + (m |> read p1 |> int) }
-    |> incrementPC 2
-
-let private exInput m =
+let private readInput pAddr1 m =
     match m.Input with
-    | v::vs -> 
-        let p1 = m |> getParamAddr 1
-        { m with Input = vs }
-        |> write p1 v
-        |> incrementPC 2
+    | v::vs -> m |> write pAddr1 v |> setInput vs |> incrementPC 2
     | _ -> failwith "No input"
-    
-let private exOutput m =
-    let p1 = m |> getParamAddr 1
-    { m with Output = (m |> read p1)::m.Output }
-    |> incrementPC 2
+
+let private writeOutput pAddr1 m =
+    m |> setOutput ((m |> read pAddr1)::m.Output) |> incrementPC 2
+
+let private offsetBase pAddr1 m =
+    { m with RelOffset = m.RelOffset + (m |> read pAddr1 |> int) } |> incrementPC 2
 
 let tick m =
     match opcode m with
-    | 99 -> None
-    | 1 -> Some (exAdd m)
-    | 2 -> Some (exMult m)
-    | 3 -> Some (exInput m)
-    | 4 -> Some (exOutput m)
-    | 5 -> Some (jumpIfTrue m)
-    | 6 -> Some (jumpIfFalse m)
-    | 7 -> Some (exLessThan m)
-    | 8 -> Some (exEquals m)
-    | 9 -> Some (offsetRelativeBase m)
-    | _ -> None
+    | op when 0 < op && op < 10 ->
+        let pAddr1 = m |> getParamAddr 1
+        match op with
+        | 1 | 2 | 7 | 8 ->
+            let pAddr2 = m |> getParamAddr 2 
+            let pAddr3 = m |> getParamAddr 3
+            match op with 
+            | 1 -> add pAddr1 pAddr2 pAddr3 m
+            | 2 -> mult pAddr1 pAddr2 pAddr3 m
+            | 7 -> lt pAddr1 pAddr2 pAddr3 m
+            | 8 | _ -> eq pAddr1 pAddr2 pAddr3 m
+        | 5 | 6 ->
+            let pAddr2 = m |> getParamAddr 2
+            if op = 5
+            then jumpTrue pAddr1 pAddr2 m
+            else jumpFalse pAddr1 pAddr2 m
+        | 3 -> readInput pAddr1 m
+        | 4 -> writeOutput pAddr1 m
+        | 9 | _ -> offsetBase pAddr1 m
+        |> Some
+    | 99 | _ -> None
 
 let rec run m =
     match tick m with
@@ -108,9 +102,8 @@ let loadProgram path =
       Input = []
       Output = []
       Mem = 
-        File.ReadLines path 
-        |> Seq.map ( fun s -> s.Split ',' )
-        |> Seq.concat
+        File.ReadAllText path 
+        |> fun s -> s.Split ',' 
         |> Seq.map Int64.Parse
         |> Seq.indexed
         |> Seq.fold ( fun mem (i,v) -> mem |> Map.add i v ) Map.empty }
