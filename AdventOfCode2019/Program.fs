b2923bf20ecee6d3f8d63f9a28591e343a637b2a
Unknown <zcrites@umich.edu>
2019-12-14 17:27:54 -0500

    Update main

--- AdventOfCode2019/Program.fs
+++ AdventOfCode2019/Program.fs
@@ -13,38 +13,40 @@ let run day part (runProblem:unit->'T) =
     printfn "(%.3fs) %.3fs   Day %d Part %d = %O" total time day part result
 
 [<EntryPoint>]
-let main argv =
+let main _ =
 
    programTimer.Start ()
-   
-   run 1 1 Day1.part1
-   run 1 2 Day1.part2
-   run 2 1 Day2.part1
-   run 2 2 Day2.part2
-   run 3 1 Day3.part1
-   run 3 2 Day3.part2
-   run 4 1 Day4.part1
-   run 4 2 Day4.part2
-   run 5 1 Day5.part1
-   run 5 2 Day5.part2
-   run 6 1 Day6.part1
-   run 6 2 Day6.part2
-   run 7 1 Day7.part1
-   run 7 2 Day7.part2
-   run 8 1 Day8.part1
-   run 8 2 Day8.part2
-   run 9 1 Day9.part1
-   run 9 2 Day9.part2
-   run 10 1 Day10.part1
-   run 10 2 Day10.part2
-   run 11 1 Day11.part1
-   run 11 2 Day11.part2
-   run 12 1 Day12.part1
-   run 12 2 Day12.part2
-   run 13 1 Day13.part1
-   run 13 2 Day13.part2
-   run 14 1 Day14.part1
-   run 14 2 Day14.part2
+
+   [ Day1.part1 >> sprintf "%d"
+     Day1.part2 >> sprintf "%d"
+     Day2.part1 >> sprintf "%d"
+     Day2.part2 >> sprintf "%d"
+     Day3.part1 >> sprintf "%d"
+     Day3.part2 >> sprintf "%d"
+     Day4.part1 >> sprintf "%d"
+     Day4.part2 >> sprintf "%d"
+     Day5.part1 >> sprintf "%d"
+     Day5.part2 >> sprintf "%d"
+     Day6.part1 >> sprintf "%d"
+     Day6.part2 >> sprintf "%d"
+     Day7.part1 >> sprintf "%d"
+     Day7.part2 >> sprintf "%d"
+     Day8.part1 >> sprintf "%d"
+     Day8.part2
+     Day9.part1 >> sprintf "%d"
+     Day9.part2 >> sprintf "%d"
+     Day10.part1 >> sprintf "%d"
+     Day10.part2 >> sprintf "%d"
+     Day11.part1 >> sprintf "%d"
+     Day11.part2
+     Day12.part1 >> sprintf "%d"
+     Day12.part2 >> sprintf "%d"
+     Day13.part1 >> sprintf "%d"
+     Day13.part2 >> sprintf "%d"
+     Day14.part1 >> sprintf "%d"
+     Day14.part2 >> sprintf "%d" ]
+   |> List.indexed 
+   |> List.iter (fun (i, part) -> run (1 + i / 2) (1 + i % 2) part)
 
    System.Console.ReadLine () |> ignore
    
