f1e21e093219145b6bd39d899ec660f6faeb9cbe
Zachary Crites <z.crites@techsmith.com>
2019-12-17 12:32:29 -0500

    Day 17 part 1

---
+++ AdventOfCode2019/Day17.fs
@@ -0,0 +1,27 @@
+﻿module Day17
+
+open Computer
+
+let scaffoldMap = 
+    loadProgram "Input/day17.txt"
+    |> outputSeq
+    |> Seq.map (fun (v,_) -> char v)
+    |> Seq.scan( fun ((x,y),last) c -> 
+       if last = '\n' 
+       then (0, y+1), c
+       else (x+1, y), c ) ((0,-1),'\n')
+    |> Seq.filter (fun (_,c) -> c <> '\n' )
+    |> Map.ofSeq
+
+let neighbors (x,y) = [ (x-1,y); (x+1,y); (x,y-1); (x,y+1) ]
+
+let part1 () =
+   scaffoldMap
+   |> Map.toSeq
+   |> Seq.filter (fun (pos,_) ->
+      neighbors pos 
+      |> List.exists( fun n -> (Map.tryFind n scaffoldMap) <> Some '#' )
+      |> not )
+   |> Seq.toList
+   |> Seq.map ( fun ((x,y),_) -> x * y )
+   |> Seq.sum
