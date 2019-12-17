29a33e34884963744e73111b1cbb77b9e3a91790
Unknown <zcrites@umich.edu>
2019-12-16 21:47:41 -0500

    Pretty slow day 16

--- AdventOfCode2019/Day16.fs
+++ AdventOfCode2019/Day16.fs
@@ -5,36 +5,51 @@ open System.IO
 
 let input =
    File.ReadAllText "Input/day16.txt"
-   |> Seq.choose (fun c ->
-      match Int32.TryParse (sprintf "%c" c) with
-      | true, n -> Some n
-      | _ -> None )
-   |> Seq.toList
+   |> Seq.map (fun c -> int c - int '0')
+   |> Seq.filter (fun c -> 0 <= c && c < 10)
+   |> Seq.toArray
 
 let input100 =
-   Seq.replicate 100 input
+   Seq.replicate 10000 input
    |> Seq.concat
-   |> Seq.toList
+   |> Seq.toArray
 
-let rec phase n = seq {
-   for v in [0;1;0;-1] do
-      yield! Seq.replicate n v
-   yield! phase n }
+let fft (signal:int array) =
+   let sums =
+      (Array.scan (+) 0 signal) 
 
-let fft (signal:int list) =
    [1..signal.Length]
-   |> Seq.map( fun outputIndex ->
-      Seq.zip
-         signal
-         (phase outputIndex |> Seq.skip 1)
-      |> Seq.map (fun (v,p) -> v * p )
+   |> Seq.map( fun phase ->
+      [(phase-1)..(phase*4)..(signal.Length-1)]
+      |> Seq.map ( fun s1 ->
+         let s2 = (s1 + phase) |> min (signal.Length)
+         let s3 = (s1 + (2 * phase)) |> min (signal.Length)
+         let s4 = (s1 + (3 * phase)) |> min (signal.Length)
+      
+         let positiveSum = sums.[s2] - sums.[s1] 
+         let negativeSum = sums.[s4] - sums.[s3] 
+         positiveSum - negativeSum )
       |> Seq.sum )
    |> Seq.map ( fun v -> abs v % 10 )
-   |> Seq.toList
+   |> Seq.toArray
 
 let part1 () =
    [1..100]
-   |> Seq.fold ( fun s _ -> fft s ) input
+   |> Seq.fold (fun s _ -> fft s ) input
    |> Seq.take 8
    |> Seq.map (sprintf "%d")
    |> fun digits -> String.Join( "", Array.ofSeq digits )
+
+let part2 () =
+    let offset = 
+        input
+        |> Seq.take 7
+        |> Seq.fold (fun r v -> r * 10 + v ) 0
+
+    [1..100]
+    |> Seq.fold ( fun s i -> fft s ) input100
+    |> Seq.skip (int offset)
+    |> Seq.take 8
+    |> Seq.map (sprintf "%d")
+    |> fun digits -> String.Join( "", Array.ofSeq digits )
+   
